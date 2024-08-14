d76514672080a4975927b1c4f00bf76751e91470
E <112919165+afkbotting@users.noreply.github.com>
2024-08-14 18:19:44 -0400

    Update injection.js

--- injection.js
+++ injection.js
@@ -963,7 +963,7 @@ function modifyCode(text) {
 
 			const survival = new Module("SurvivalMode", function(callback) {
 				if(callback) {
-					if(player$1) player$1.setGamemode(GameMode.fromId("creative"));
+					if(player$1) player$1.setGamemode(GameMode.fromId("spectator"));
 					survival.toggle();
 				}
 			});
